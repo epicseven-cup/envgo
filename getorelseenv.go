56d4f9e8a8684f7bf67e49616284ddf23c47d2c0
jacky <jackylin616@gmail.com>
2025-10-25 16:21:29 -0500

    adds some type sconverstion

--- getorelseenv.go
+++ getorelseenv.go
@@ -1,21 +1,57 @@
 package getorelseenv
 
-import "os"
+import (
+	"errors"
+	"os"
+	"strconv"
+)
 
-// GetValueOrDefault retrieves a value from the environment variable with the given key.
+// GetValueOrDefault retrieves the value of the environment variable specified by key.
+// If the variable is not set or is an empty string, it returns the defaultValue.
 //
-// Args:
+// The generic type T is used to handle general type conversion (e.g., string to int).
+// It returns an error if type conversion fails for a non-empty environment variable.
 //
-//	key: The name of the environment variable to retrieve.
-//	value: The default value to return if the environment variable is not set.
+// # By default when error, it returns the defaultValue and the error
 //
-// Returns:
+// Example:
 //
-//	The value of the environment variable if it is set, otherwise the
-func GetValueOrDefault(key string, value string) string {
+//	intValue, err := GetValueOrDefault[int]("MY_INT_VAR", 0)
+func GetValueOrDefault[T any](key string, defaultValue T) (T, error) {
 	v, ok := os.LookupEnv(key)
 	if !ok {
-		return value
+		return defaultValue, nil
 	}
-	return v
+
+	// Checking what to convert to base on the defaultValue type
+	switch any(defaultValue).(type) {
+	case string:
+		return any(v).(T), nil
+	case int:
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			return defaultValue, err
+		}
+		return any(n).(T), nil
+	case float32:
+		f32, err := strconv.ParseFloat(v, 32)
+		if err != nil {
+			return defaultValue, err
+		}
+		return any(f32).(T), nil
+	case float64:
+		f64, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return defaultValue, err
+		}
+		return any(f64).(T), nil
+	case bool:
+		b, err := strconv.ParseBool(v)
+		if err != nil {
+			return defaultValue, err
+		}
+		return any(b).(T), nil
+	default:
+		return defaultValue, errors.New("No Matching convseration type found")
+	}
 }
